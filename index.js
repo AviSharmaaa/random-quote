5a2876f9f9478382cb6fff74bd6a14c0a16c00d3
Avi Sharma <71590361+AviSharma1305@users.noreply.github.com>
2022-07-15 17:56:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let quoteText = document.getElementById('quote-text');
+let quoteBtn = document.querySelector('.btn');
+let quoteAuthor = document.getElementById('author');
+let reload = document.getElementById('reload');
+let quoteUrl = 'https://api.quotable.io/random';
+
+let getQuote = async () => {
+    quoteText.innerText ='...Loading';
+    quoteBtn.classList.add('fa-spin');
+    await fetch(quoteUrl).then(res => res.json()).then(result => {
+
+        let randomQuote = result.content;
+        let author = result.author;
+        console.log(quoteText);
+
+        quoteText.innerText = randomQuote;
+        quoteAuthor.innerText = '~' + author;
+        console.log(randomQuote);
+        console.log(author);
+        quoteBtn.classList.remove('fa-spin');
+
+    })
+};
+
+reload.addEventListener('click',getQuote);
